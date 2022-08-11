71a25a78b5ad11eea3c2963a0224a891e0336344
shella12 <ayeshaarshad4567@gmail.com>
2022-08-11 13:34:45 +0500

    Add Luxon module

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { default as bookAdd , bookArray } from './JS/bookStorage.js';
 import addNewList from './JS/add-books.js';
+import updateDate from './JS/date.js';
+
+updateDate();
 
 const form=document.querySelector('.form');
-
 form.addEventListener('submit',(event)=>{
   event.preventDefault();
   const [title,author]= [form.title.value,form.author.value];
