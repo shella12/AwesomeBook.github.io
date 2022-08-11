b2c34d8e0b9a2165fa8c37d13e4c5c8d41368714
shella12 <ayeshaarshad4567@gmail.com>
2022-08-11 18:43:06 +0500

    Add Pages and section modules

---
+++ JS/section.js
@@ -0,0 +1,13 @@
+const section=document.body.querySelectorAll('section');
+console.log(section);
+const sectionSwitch =(id)=>{
+
+    section.forEach((element,index)=>{
+      if(index===id){
+        element.classList.add('active');
+      }
+        
+    });
+}
+
+export default sectionSwitch;
--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import { default as bookAdd , bookArray } from './JS/bookStorage.js';
 import addNewList from './JS/add-books.js';
 import updateDate from './JS/date.js';
+import pages from './JS/pages.js';
 
-updateDate();
 
-const form=document.querySelector('.form');
+
+const navBarLinks=document.body.querySelector('.navbar-ulist');
+navBarLinks.addEventListener('click',(event)=>{
+pages(event);
+});
+
+const form=document.body.querySelector('.form');
 form.addEventListener('submit',(event)=>{
   event.preventDefault();
   const [title,author]= [form.title.value,form.author.value];
   bookAdd(title,author);
   addNewList();
 });
+
+updateDate();
